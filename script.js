7b3f994bfd6477c35e8fdab243a02b22525e2b21
Sarath O S <sarathos584@gmail.com>
2023-02-18 17:24:25 +0530

    added sound to piano first time on 18th feb 2023

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
 
-let audio = new Audio("")
+let audio = new Audio("./assets/a.wav");  //  by default, audio src is "a" tune 
 
-const playTune = (key)=> {
+const playTune = (key)=> { 
+    audio.src = `./assets/${key}.wav` // passing audio src based on key pressed 
+    audio.play();  // playing audio
 
+    
 }
 
 pianoKeys.forEach(key => {
     // calling playTune function with passing dat-key value as an argument
     key.addEventListener('click', ()=> playTune(key.dataset.key));
 } );
+
